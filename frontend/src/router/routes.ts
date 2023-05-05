7c51c19286352c4f305628f9dacd6a990a889282
LiaoAnn <1050123tom@gmail.com>
2023-05-06 00:12:54 +0800

    prettier&eslint

---
+++ frontend/src/router/routes.ts
@@ -0,0 +1,17 @@
+import { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('../layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('../pages/HomeView.vue')
+      }
+    ]
+  }
+]
+
+export default routes

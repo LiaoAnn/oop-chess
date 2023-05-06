b6447bc1650da60c12845cc7513f2ff5cabc86aa
LiaoAnn <1050123tom@gmail.com>
2023-05-06 14:17:56 +0800

    feat: added some useful component to app; added some setting

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,45 @@
 <template>
-  <RouterView />
+  <n-config-provider :theme="theme" :theme-overrides="themeOverrdes">
+    <n-dialog-provider>
+      <n-loading-bar-provider>
+        <n-message-provider>
+          <n-notification-provider>
+            <router-view />
+          </n-notification-provider>
+        </n-message-provider>
+      </n-loading-bar-provider>
+    </n-dialog-provider>
+  </n-config-provider>
 </template>
+
+<script setup lang="ts">
+import '@master/css';
+
+import {
+  GlobalThemeOverrides,
+  NConfigProvider,
+  NDialogProvider,
+  NLoadingBarProvider,
+  NMessageProvider,
+  NNotificationProvider,
+} from 'naive-ui';
+import { computed, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+console.log(route.params);
+
+//#region Theme
+const theme = ref<(typeof NConfigProvider)['theme']>('light');
+const basicThemeOverrides: GlobalThemeOverrides = {
+  common: {
+    primaryColor: '#632d13',
+    fontWeightStrong: '600',
+  },
+};
+
+const themeOverrdes = computed(() => {
+  return basicThemeOverrides;
+});
+//#endregion
+</script>

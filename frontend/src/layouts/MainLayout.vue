bc502d146999542fea42086696104e60d84ea298
LiaoAnn <1050123tom@gmail.com>
2023-05-07 23:22:28 +0800

    feat: finished site-footer

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -2,11 +2,13 @@
   <n-layout>
     <site-header />
     <router-view />
+    <site-footer />
   </n-layout>
 </template>
 
 <script setup lang="ts">
 import { NLayout } from 'naive-ui';
 
+import SiteFooter from '@/components/SiteFooter.vue';
 import SiteHeader from '@/components/SiteHeader.vue';
 </script>

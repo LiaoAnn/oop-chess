96748e7df61d4e427e93aed08030035565516a84
yaoenliu <peterliu1060@gmail.com>
2023-05-15 17:08:11 +0800

    added example implementation of sending and receiving of websocket

--- backend/backend.cpp
+++ backend/backend.cpp
@@ -1,20 +1,49 @@
-﻿// backend.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿#include <iostream>
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
 
-#include <iostream>
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+typedef server::message_ptr message_ptr;
+
+void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    server echo_server;
+
+    try {
+        // Set logging settings
+        echo_server.set_access_channels(websocketpp::log::alevel::all);
+        echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+        // Initialize Asio
+        echo_server.init_asio();
+        // Register our message handler
+        echo_server.set_message_handler(bind(&on_message, &echo_server, ::_1, ::_2));
+        // Listen on port 9002
+        echo_server.listen(9002);
+        // Start the server accept loop
+        echo_server.start_accept();
+        // Start the ASIO io_service run loop
+        echo_server.run();
+    }
+    catch (websocketpp::exception const& e) {
+        std::cout << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cout << "other exception" << std::endl;
+    }
 }
 
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
+void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
+    // recive message from client
+    std::cout << msg->get_payload() << std::endl;
+    // send message to client
+    std::string str = "Server received: " + msg->get_payload();
+    s->send(hdl, str, msg->get_opcode());
+}

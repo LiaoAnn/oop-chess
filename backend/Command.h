435a97ce3d4c11a4afd4fea0e8cd666e72de446b
binga288 <67582826+binga288@users.noreply.github.com>
2023-05-16 00:09:43 +0800

    Command operation preparation

---
+++ backend/Command.h
@@ -0,0 +1,89 @@
+/***********************************************************************
+ * File: Command.h
+ * Author: BING-JIA TAN (B11115001)
+ * Create Date: 2023-05-15
+ * Editor: BING-JIA TAN (B11115001)
+ * Update Date: 2023-05-15
+ * Description: Describe here
+************************************************************************/
+#pragma once
+
+#include <iostream>
+
+// 前->後 指令範例
+/*
+*  1. 初始化
+*	{
+*		type:"init",
+*	}
+*	{
+*		success:true,
+*		board:[
+*			{
+*				type:"", // king、queen、rook、bishop、knight、pawn
+*				color:"black", // black、white
+*				position:"A4",
+*			},
+*			...
+*		]
+*	}
+*  2. 點格子
+*	// 分為兩種情況 1. 點選自己的棋子 2. 點選空格 開全域存
+*   {
+*		type:"click",
+*       position:"{pos}", // 例如: "A2"
+*   }
+*	{
+*		success:true,
+*		player:"black", // black、white
+*
+*	}
+*  3. 認輸
+*   {
+*		type:"surrender",
+*   }
+*  4. 上一步
+*	{
+*		 type:"undo",
+*	}
+*	{
+*		success:true,
+*		board:[
+*			{
+*				type:"", // king、queen、rook、bishop、knight、pawn
+*				color:"black", // black、white
+*				position:"A4",
+*			},
+*			...
+*		]
+*	}
+*  5. 存檔
+*	{
+*		type:"save",
+*	}
+*  6. 讀檔
+*	{
+*		type:"load",
+*	}
+*	{
+*		success:true,
+*		board:[
+*			{
+*				type:"", // king、queen、rook、bishop、knight、pawn
+*				color:"black", // black、white
+*				position:"A4",
+*			},
+*			...
+*		]
+*	}
+*/
+
+// 紀錄表
+/*
+ *
+*/
+
+
+
+
+

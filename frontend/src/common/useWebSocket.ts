e99637db82b9438ad260d2182bec5d258e84b650
yaoenliu <peterliu1060@gmail.com>
2023-05-17 20:59:50 +0800

    feat: finished moving and  valid-position and surrender

---
+++ frontend/src/common/useWebSocket.ts
@@ -0,0 +1,55 @@
+export enum SendMsgType {
+  Init = 'init',
+  Click = 'click',
+  Surrender = 'surrender',
+}
+
+export enum ReceiveMsgType {
+  Move = 'move',
+  FirstClick = 'firstClick',
+  Win = 'win',
+  Surrender = 'surrender',
+  ValidMoves = 'validMoves',
+}
+
+export interface SendMsg {
+  type: SendMsgType;
+  position?: string;
+}
+
+export interface ReceiveMsg {
+  type: ReceiveMsgType;
+  success?: boolean;
+  from?: string;
+  to?: string;
+  player?: string;
+  position?: string;
+  validMoves?: {
+    x: number;
+    y: number;
+  }[];
+}
+
+export function useWebSocket(onMsgCallback: (e: MessageEvent) => void) {
+  const ws = new WebSocket('ws://localhost:9002');
+  const openWs = async () => {
+    return new Promise((resolve, reject) => {
+      if (ws.readyState === WebSocket.OPEN) {
+        resolve(ws);
+      }
+      ws.onopen = () => {
+        resolve(ws);
+      };
+      ws.onerror = (e) => {
+        reject(e);
+      };
+    });
+  };
+  const sendMsg = async (msg: SendMsg) => {
+    await openWs();
+    ws.send(JSON.stringify(msg));
+  };
+  ws.onmessage = onMsgCallback;
+
+  return { openWs, sendMsg };
+}
